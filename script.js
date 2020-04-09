e05006545136e61c0322f2ae5fef58880978a78a
anastasiakibkalo <kibkaloanastasiya@gmail.com>
2020-04-10 01:56:55 +0200

    edit todo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-let svg = '<svg version="1.1" class="input__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="195.248px" height="195.248px" viewBox="0 0 195.248 195.248" style="enable-background:new 0 0 195.248 195.248;" xml:space="preserve"> <g> <path class="svg__delete" d="M193.063,155.572c0.334-1.679,0.039-3.517-1.278-5.161c-14.991-18.703-31.115-36.796-48.107-54.137 C149.784,89.579,155.83,82.828,161.75,76c9.912-11.433,24.994-26.328,26.968-41.827c0.093-0.726-0.042-1.349-0.263-1.9 c0.05-0.163,0.143-0.292,0.176-0.468c0.271-1.439,0.134-2.545-0.146-3.989c0.396,2.055-0.376-2.196-0.683-2.72 c-0.506-0.864-1.346-1.691-1.979-2.49c-2.536-3.197-5.292-6.226-8.112-9.173C174.046,9.58,166.176,0.74,159.893,2.962 c-1.828-2.272-5.151-3.214-7.646-0.394c-16.481,18.629-35.146,34.924-54.119,50.89C80.167,37.949,61.535,23.322,42.385,9.805 c-1.07-0.755-2.192-0.374-2.916,0.419c-0.971-0.068-1.969,0.145-2.896,0.77c-4.731,3.192-8.779,7.646-12.932,11.548 c-5.044,4.739-10.211,9.354-15.32,14.024c-0.674,0.617-1.056,1.338-1.217,2.088c-2.354,0.614-3.573,3.956-0.901,5.631 c19.087,11.966,35.1,27.953,49.914,45.069c-1.992,1.79-4.014,3.548-5.979,5.367c-13.436,12.439-26.681,25.389-37.835,39.958 C7.38,141.11-3.837,154.976,1.33,163.811c1.047,1.791,3.817,1.855,4.849,0c0.132-0.237,0.283-0.46,0.416-0.696 c2.292,4.629,6.075,8.898,9.355,12.25c4.495,4.591,11.863,12.046,18.964,11.42c0.387,1.837,2.996,2.569,4.348,1.191 c12.475-12.719,23.739-24.017,38.603-34.358c6.517-4.534,13.184-9.018,19.7-13.704c9.373,11.205,19.061,22.144,29.732,32.137 c9.274,8.686,22.869,21.983,36.165,22.373c1.608,0.047,2.812-0.655,3.557-1.697c7.865-3.384,15.068-9.347,20.631-15.772 C191.912,172.029,198.797,161.489,193.063,155.572z M42.891,61.424c1.146,0.615,2.304,1.214,3.48,1.786 c0.17,0.153,0.341,0.303,0.511,0.456c0.141,0.388,0.277,0.777,0.54,1.138c0.499,0.684,1.025,1.344,1.53,2.023 C46.96,64.987,44.941,63.185,42.891,61.424z M27.382,168.658c-0.544,0.356-0.988,0.785-1.348,1.255 c-0.294-0.262-0.584-0.526-0.874-0.791c1.173-0.771,2.319-1.573,3.47-2.367c0.335,0.103,0.677,0.171,1.023,0.215 c0.036,0.057,0.077,0.108,0.115,0.162C28.977,167.644,28.171,168.141,27.382,168.658z M116.361,108.108 c-0.651-0.487-1.304-0.975-1.949-1.47c-2.056-2.269-4.118-4.529-6.19-6.78c2.744,2.721,5.521,5.405,8.354,8.028 C116.504,107.959,116.434,108.035,116.361,108.108z"/> </g> </svg>'
+let svgDelete = '<svg version="1.1" class="input__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="195.248px" height="195.248px" viewBox="0 0 195.248 195.248" style="enable-background:new 0 0 195.248 195.248;" xml:space="preserve"> <g> <path class="svg__delete" d="M193.063,155.572c0.334-1.679,0.039-3.517-1.278-5.161c-14.991-18.703-31.115-36.796-48.107-54.137 C149.784,89.579,155.83,82.828,161.75,76c9.912-11.433,24.994-26.328,26.968-41.827c0.093-0.726-0.042-1.349-0.263-1.9 c0.05-0.163,0.143-0.292,0.176-0.468c0.271-1.439,0.134-2.545-0.146-3.989c0.396,2.055-0.376-2.196-0.683-2.72 c-0.506-0.864-1.346-1.691-1.979-2.49c-2.536-3.197-5.292-6.226-8.112-9.173C174.046,9.58,166.176,0.74,159.893,2.962 c-1.828-2.272-5.151-3.214-7.646-0.394c-16.481,18.629-35.146,34.924-54.119,50.89C80.167,37.949,61.535,23.322,42.385,9.805 c-1.07-0.755-2.192-0.374-2.916,0.419c-0.971-0.068-1.969,0.145-2.896,0.77c-4.731,3.192-8.779,7.646-12.932,11.548 c-5.044,4.739-10.211,9.354-15.32,14.024c-0.674,0.617-1.056,1.338-1.217,2.088c-2.354,0.614-3.573,3.956-0.901,5.631 c19.087,11.966,35.1,27.953,49.914,45.069c-1.992,1.79-4.014,3.548-5.979,5.367c-13.436,12.439-26.681,25.389-37.835,39.958 C7.38,141.11-3.837,154.976,1.33,163.811c1.047,1.791,3.817,1.855,4.849,0c0.132-0.237,0.283-0.46,0.416-0.696 c2.292,4.629,6.075,8.898,9.355,12.25c4.495,4.591,11.863,12.046,18.964,11.42c0.387,1.837,2.996,2.569,4.348,1.191 c12.475-12.719,23.739-24.017,38.603-34.358c6.517-4.534,13.184-9.018,19.7-13.704c9.373,11.205,19.061,22.144,29.732,32.137 c9.274,8.686,22.869,21.983,36.165,22.373c1.608,0.047,2.812-0.655,3.557-1.697c7.865-3.384,15.068-9.347,20.631-15.772 C191.912,172.029,198.797,161.489,193.063,155.572z M42.891,61.424c1.146,0.615,2.304,1.214,3.48,1.786 c0.17,0.153,0.341,0.303,0.511,0.456c0.141,0.388,0.277,0.777,0.54,1.138c0.499,0.684,1.025,1.344,1.53,2.023 C46.96,64.987,44.941,63.185,42.891,61.424z M27.382,168.658c-0.544,0.356-0.988,0.785-1.348,1.255 c-0.294-0.262-0.584-0.526-0.874-0.791c1.173-0.771,2.319-1.573,3.47-2.367c0.335,0.103,0.677,0.171,1.023,0.215 c0.036,0.057,0.077,0.108,0.115,0.162C28.977,167.644,28.171,168.141,27.382,168.658z M116.361,108.108 c-0.651-0.487-1.304-0.975-1.949-1.47c-2.056-2.269-4.118-4.529-6.19-6.78c2.744,2.721,5.521,5.405,8.354,8.028 C116.504,107.959,116.434,108.035,116.361,108.108z"/> </g> </svg>'
+// let svgPlus = '<svg version="1.1" class="input__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="187.021px" height="187.02px" viewBox="0 0 187.021 187.02" style="enable-background:new 0 0 187.021 187.02;" xml:space="preserve"> <g> <path class="svg__plus" d="M185.132,94.379c0.233-4.415,0.699-9.701-0.188-14.409c2.227-1.899,3.053-5.695,0.505-7.693 c-14.683-11.514-39.473-11.648-60.876-10.318c-0.302-19.734-1.954-40.196-8.038-57.822c-1.495-4.331-7.405-4.797-9.596-1.641 c-0.323-0.092-0.591-0.241-0.954-0.289C97.646,1.11,88.299,2.172,79.902,2.62c-2.523,0.134-3.986,1.725-4.424,3.562 c-0.66-0.03-1.321-0.07-1.98-0.092c-0.453-0.015-0.853,0.068-1.208,0.214c-1.883-0.359-3.87,0.212-4.741,2.241 c-7.503,17.49-8.649,38.542-8.587,58.557c-15.109,1.175-30.224,1.955-45.389,1.371c-1.842-2.275-5.83-2.428-8.501-0.477 c-0.654-0.049-1.307-0.079-1.962-0.134c-3.013-0.253-3.607,4.568-0.708,5.231c0.007,0.001,0.014,0.003,0.021,0.004 c-2.065,15.594-4.339,30.9,0.263,46.238c-0.29,0.321-0.59,0.633-0.84,0.986c-0.498,0.703-0.068,1.56,0.525,2.002 c7.254,5.408,21.293,0.716,29.771-0.119c9.337-0.919,18.696-1.655,28.061-2.292c0.819,19.396,2.094,38.832,3.909,58.015 c-0.208,1.947,0.746,4.166,2.858,4.92c8.1,2.896,16.372,3.693,24.932,3.672c6.697-0.017,16.443,0.775,21.962-3.487 c1.003-0.774,1.636-1.71,1.986-2.686c1.97,0.522,4.336-0.568,4.591-3.34c1.789-19.581,2.711-39.349,3.297-59.092 c17.519,0.338,34.985,1.705,52.301,4.793c0.99,0.575,2.169,0.744,3.273,0.464c5.823,0.312,8.583-7.365,4.507-10.376 C184.704,106.699,184.817,100.34,185.132,94.379z"/> </g> </svg>'
+let svgEditor = '<svg version="1.1" class="input__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 441.778 441.778" style="enable-background:new 0 0 441.778 441.778;" xml:space="preserve"> <path class="svg__editor" d="M429.719,12.062C421.941,4.284,411.587,0,400.564,0c-11.022,0-21.376,4.284-29.154,12.062l-26.339,26.34 c-3.904-3.905-10.234-3.905-14.143,0c-3.905,3.905-3.905,10.237,0,14.142l-21.213,21.213c-3.904-3.905-10.234-3.905-14.143,0 c-3.905,3.905-3.905,10.237,0,14.142L45.358,338.113c-0.006,0.006-0.011,0.011-0.017,0.017c-0.001,0.001-0.002,0.001-0.002,0.002 c-0.852,0.856-1.516,1.827-1.994,2.863L20.53,387.564c-0.071,0.138-0.14,0.277-0.205,0.418L1.023,427.378 c-1.881,3.839-1.113,8.448,1.909,11.471c1.92,1.919,4.479,2.929,7.074,2.929c1.492,0,2.996-0.334,4.396-1.021l39.391-19.299 c0.145-0.066,0.287-0.137,0.43-0.21l46.564-22.814c1.035-0.478,2.005-1.142,2.86-1.992c0.001-0.001,0.002-0.001,0.002-0.002 c0.006-0.006,0.012-0.011,0.018-0.017l250.214-250.214c3.906,3.906,10.236,3.906,14.143,0c3.905-3.905,3.905-10.237,0-14.143 l21.213-21.213c3.906,3.906,10.236,3.906,14.143,0c3.905-3.905,3.905-10.237,0-14.142l26.34-26.34 C445.794,54.295,445.794,28.138,429.719,12.062z M31.835,409.945l0.396-0.808l0.412,0.412L31.835,409.945z M51.626,400.249 l-10.095-10.094l13.721-28.004l24.378,24.378L51.626,400.249z M96.596,375.209l-30.024-30.025l243.144-243.143l30.024,30.025 L96.596,375.209z M353.881,117.924l-30.024-30.025l21.213-21.213l30.025,30.025L353.881,117.924z M415.577,56.229l-26.34,26.34 l-30.024-30.025l26.34-26.34c4-4,9.332-6.204,15.012-6.204c5.681,0,11.013,2.203,15.013,6.204 C423.855,34.482,423.855,47.951,415.577,56.229z"/> </svg> '
 let enterBtn = document.querySelector('.input__btn');
 let input = document.querySelector('.input__new');
 let ul = document.querySelector('.todo__list');
+let todoTextWidth = document.querySelector('.todo__text-width');
 let item = document.getElementsByTagName('li');
 
 function inputLength(){
@@ -13,42 +16,114 @@ function listLength(){
 	return item.length;
 }
 
+function widthInput() {
+  todoTextWidth.innerHTML = input.value;
+}
+
+input.addEventListener('input', widthInput);
+
 function createListElement() {
   
+  //todo add item input
   let todoItem = document.createElement('li');
-  
   todoItem.className = "todo__item";
 
-  // let todoName =  document.createElement('input');
-  // todoName.value = input.value;
-  let todoName =  document.createElement('span');
-  todoName.innerHTML = input.value;
+  let todoWrapper = document.createElement('div');
+  todoWrapper.className = 'todo__text-wrapper';
+  todoWrapper.style.width = (todoTextWidth.offsetWidth + 5)+ 'px';
+
+  let todoName =  document.createElement('input');
+  todoName.value = input.value;
+  todoName.style.width = (todoTextWidth.offsetWidth + 5)+ 'px';
+  todoName.disabled = true;
   todoName.className = "todo__text";
 
-  todoItem.append(todoName);
+ 
+  todoWrapper.append(todoName);
+  todoItem.append(todoWrapper);
+  ul.append(todoItem);
 
-	ul.append(todoItem);
+  
+  
 	input.value = "";
 
-	function crossOut() {
-		todoName.classList.toggle("item__done");
+
+  //todo add button
+  let todoBtn = document.createElement('div');
+  todoBtn.className = "todo__btn-wrapper";
+
+  let deleteBtn = document.createElement("span");
+  deleteBtn.className = "todo__delete";
+  deleteBtn.innerHTML = svgDelete;
+
+  // let plusBtn = document.createElement("span");
+  // plusBtn.className = "todo__plus";
+  // plusBtn.innerHTML = svgPlus;
+
+  let editorBtn = document.createElement("span");
+  editorBtn.className = "todo__editor";
+  editorBtn.innerHTML = svgEditor;
+
+  // todoBtn.append(plusBtn);
+  todoBtn.append(editorBtn);
+  todoBtn.append(deleteBtn);
+  todoItem.append(todoBtn);
+
+
+
+  //Item done
+  function crossOut() {
+    if (todoName.disabled === true) {
+      todoWrapper.classList.toggle("item__done");
+    }
 	}
+	todoWrapper.addEventListener("click",crossOut);
 
-	todoName.addEventListener("click",crossOut);
-  
-  let dBtn = document.createElement("span");
-  dBtn.className = "todo__delete";
-  dBtn.innerHTML = svg;
-  todoItem.append(dBtn);
-  dBtn.addEventListener('click', deleteListItem);
 
+
+  //item delete
 	function deleteListItem(){
-
+    todoName.disabled = true;
     todoItem.classList.add("todo__item-delete");
     setTimeout(() => todoItem.classList.add("delete"), 700);
-		// todoItem.classList.add("delete")
   }
+  deleteBtn.addEventListener('click', deleteListItem);
+
+
+  //input edit
+  function inputEdit() {
+    // todoName.blur();
+    todoName.toggleAttribute('disabled');
+    todoWrapper.classList.remove("item__done");
+
+    if(todoName.disabled == false) {
+      todoName.focus();
+      todoName.addEventListener('blur', inputBlur);
+
+      // editorBtn.addEventListener('click', inputEdit)
+    }
+  }
+
+  editorBtn.addEventListener('click', inputEdit)
+
   
+  todoName.addEventListener('input', () => {
+    todoTextWidth.innerHTML = todoName.value;
+    todoName.style.width = (todoTextWidth.offsetWidth + 10)+ 'px';
+    todoWrapper.style.width = (todoTextWidth.offsetWidth + 10)+ 'px';
+
+    todoName.addEventListener('keydown', function(e) {
+      if (e.keyCode === 13) {
+        todoName.disabled = true;
+      }
+    });
+  })
+
+  function inputBlur() {
+    todoName.disabled = true;
+  }
+
+
 }
 
 function addListAfterClick(){
